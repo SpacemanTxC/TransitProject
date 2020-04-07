7edca5e1b43458a1c280bc7178f3589774432aa2
Tate Capra <tatecapra@outlook.com>
2020-04-07 18:21:55 -0500

    added basic layour for java file

---
+++ TransitCalculator.java
@@ -0,0 +1,20 @@
+public class TransitCalculator{
+
+  // people can use the transit 30 times
+  int numberOfDays = 30;
+
+  //track the number of people
+  int numberOfPeople = 0;
+
+  // constructor
+  public TransitCalulator(numberOfDays, numberOfPeople){
+    numberOfDays = numberOfDays--;
+    numberOfPeople = numnerOfPeople++;
+  }
+
+  //main
+  public static void main(String[] args){
+    String[] rideOption = {"Pay-per-ride","7-day Unlimited Rides", "30-day Unlimited Rides" };
+
+  }
+}
